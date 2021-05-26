f8f9ac99d8630b1ee73e52b9289ced400ba800d0
asairaj22 <asairaj22@gmail.com>
2021-05-26 16:44:11 +0530

    Client Push Notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,24 +15,43 @@ export class AppComponent implements OnInit {
   constructor(private apiService: ApiService, private swUpdate: SwUpdate) { }
 
   ngOnInit() {
+    Notification.requestPermission(function (status) {
+      console.log('Notification permission status:', status);
+    });
+
+    function displayNotification() {
+      if (Notification.permission == 'granted') {
+        navigator.serviceWorker.getRegistration().then(function (reg: any) {
+          var options = {
+            body: 'New version Updated',
+            icon: 'assets/Images-pwa/Singtel_billing_footer_logo.png',
+            vibrate: [100, 50, 100],
+            actions: [{ action: 'close', title: 'Close' }]
+          };
+          reg.showNotification('Hello world!', options);
+        });
+      }
+    }
+
     console.log(this.swUpdate)
     if (this.swUpdate.isEnabled) {
       console.log(this.swUpdate.isEnabled)
       this.swUpdate.available.subscribe(() => {
-        window.location.reload();
+        // window.location.reload();
         // if(confirm("New version available. Load New Version?")) {
         //   window.location.reload();
         // }
+        displayNotification();
+        window.location.reload();
       });
-    } 
+    }
 
-
-    this.apiService.getNews(this.userpagedata).subscribe((data: any)=>{
+    this.apiService.getNews(this.userpagedata).subscribe((data: any) => {
       console.log(data);
       this.articles = data.data;
     });
 
-    this.apiService.getUnknown(this.userpagedata).subscribe((data: any)=>{
+    this.apiService.getUnknown(this.userpagedata).subscribe((data: any) => {
       console.log(data);
     });
 
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'pwa';
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
 const RESOURCES_TO_PRELOAD = [
 	'index.html',
 	'register-worker.js',
 	'manifest.webmanifest'
-	//'offline-404.html'
 ];
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'offline.html';
 
 /* 
    // Note: if you want to preload the UI5 core and mobile libraries by install,
@@ -20,15 +22,31 @@ const RESOURCES_TO_PRELOAD = [
 	]);
 */
 debugger;
+// Push
+self.addEventListener('notificationclick', function (e) {
+	var notification = e.notification;
+	var action = e.action;
+
+	if (action === 'close') {
+		console.log('Other than Close');
+		notification.close();
+	} else {
+		console.log('Other than Close');
+		// return clients.openWindow('/');
+	}
+});
+
+
+
 // Preload some resources during install
 self.addEventListener('install', function (event) {
 	console.log("Installed");
 	event.waitUntil(
 		caches.open(CACHE_NAME).then(function (cache) {
 			return cache.addAll(RESOURCES_TO_PRELOAD);
-		// if any item isn't successfully added to
-		// cache, the whole operation fails.
-		}).catch(function(error) {
+			// if any item isn't successfully added to
+			// cache, the whole operation fails.
+		}).catch(function (error) {
 			console.error(error);
 		})
 	);
@@ -58,27 +76,27 @@ self.addEventListener('fetch', function (event) {
 				if (response) {
 					return response; // There is a cached version of the resource already
 				}
-	
+
 				let requestCopy = event.request.clone();
 				return fetch(requestCopy).then(function (response) {
 					// opaque responses cannot be examined, they will just error
 					if (response.type === 'opaque') {
 						// don't cache opaque response, you cannot validate it's status/success
 						return response;
-					// response.ok => response.status == 2xx ? true : false;
+						// response.ok => response.status == 2xx ? true : false;
 					} else if (!response.ok) {
 						console.error(response.statusText);
 					} else {
-						return caches.open(CACHE_NAME).then(function(cache) {
+						return caches.open(CACHE_NAME).then(function (cache) {
 							cache.put(event.request, response.clone());
 							return response;
-						// if the response fails to cache, catch the error
-						}).catch(function(error) {
+							// if the response fails to cache, catch the error
+						}).catch(function (error) {
 							console.error(error);
 							return error;
 						});
 					}
-				}).catch(function(error) {
+				}).catch(function (error) {
 					// fetch will fail if server cannot be reached,
 					// this means that either the client or server is offline
 					console.error(error);
@@ -86,5 +104,29 @@ self.addEventListener('fetch', function (event) {
 				});
 			})
 		);
+	}
+	if (event.request.mode === 'navigate') {
+		event.respondWith((async () => {
+			try {
+				// First, try to use the navigation preload response if it's supported.
+				const preloadResponse = await event.preloadResponse;
+				if (preloadResponse) {
+					return preloadResponse;
+				}
+
+				const networkResponse = await fetch(event.request);
+				return networkResponse;
+			} catch (error) {
+				// catch is only triggered if an exception is thrown, which is likely
+				// due to a network error.
+				// If fetch() returns a valid HTTP response with a response code in
+				// the 4xx or 5xx range, the catch() will NOT be called.
+				console.log('Fetch failed; returning offline page instead.', error);
+
+				const cache = await caches.open(CACHE_NAME);
+				const cachedResponse = await cache.match(OFFLINE_URL);
+				return cachedResponse;
+			}
+		})());
 	}
 });
